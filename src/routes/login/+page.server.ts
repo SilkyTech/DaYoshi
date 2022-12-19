c6ea610d209755453c47a0547dbfd2b972004c2c
Silkoway <ethancai920@gmail.com>
2022-12-19 13:41:18 +0800

    not secure no more

--- src/routes/login/+page.server.ts
+++ src/routes/login/+page.server.ts
@@ -41,16 +41,10 @@ const login: Action = async ({ cookies, request }) => {
   })
 
   cookies.set('session', authenticatedUser.userAuthToken, {
-    // send cookie for every page
     path: '/',
-    // server side only cookie so you can't use `document.cookie`
     httpOnly: true,
-    // only requests from same site can send cookies
-    // https://developer.mozilla.org/en-US/docs/Glossary/CSRF
     sameSite: 'strict',
-    // only sent over HTTPS in production
-    secure: process.env.NODE_ENV === 'production',
-    // set cookie to expire after a month
+    secure: false,
     maxAge: 60 * 60 * 24 * 30,
   })
 
